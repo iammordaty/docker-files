a3829bc46473f5cfc74484dbc7e483a58150dd94
iammordaty <iammordaty@users.noreply.github.com>
2016-09-06 14:05:10 +0200

    Major changes in Assistant's docker files

--- assistant/backend/scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -fr /data/bin /data/pkg
-
-go get \
-    github.com/julienschmidt/httprouter \
-    github.com/githubnemo/CompileDaemon
-
-(cd /data/src/github.com/iammordaty/assistant-backend/ && go install )
